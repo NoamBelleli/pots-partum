159382c80ef8f779a1a6497849e1b93c629e8209
Noam Belleli <noamhoter@gmail.com>
2023-06-14 09:36:50 +0300

    initial server

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,12 @@
-console.log("hello world")
+const express = require('express');
+const dotenv = require('dotenv').config();
+const port = process.env.PORT || 5000;
+
+const app = express()
+
+app.get('/api/members', (req, res) => {
+  res.status(200).json({message: "Get members"})
+})
+
+app.listen(port, () => console.log(`Server running on port ${port}`))
+
